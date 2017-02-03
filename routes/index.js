18e30d6ba2bbd1e34fb38dbcea6facf31c42fae4
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-03 13:48:21 +0700

    merage router

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const user = require('../controllers/user');
+const middleware = require('../helpers/middleware.js');
 
 
 /* localhost:3000/api/signup | register new user  */
 router.post('/api/signup', user.registerUser);
 /* localhost:3000/api/login | login user */
-router.post('/api/login', user.loginUser);
+router.post('/api/login', middlewareo.isUser, user.loginUser);
 /* localhost:3000/api/superhero | get profile super hero*/
 router.get('/api/superhero', user.profileSuperHero);
 /* localhost:3000/api/home | go to home page */
