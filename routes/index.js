1839ee9c5d93e3ac6ad4cd96fb5a9a2cdc78e1c8
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-03 13:06:43 +0700

    create middleware, controller, router

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 const user = require('../controllers/user');
 
 
-/* register new user */
+/* localhost:3000/api/signup | register new user  */
 router.post('/api/signup', user.registerUser);
+/* localhost:3000/api/login | login user */
+router.post('/api/login', user.loginUser);
+/* localhost:3000/api/superhero | get profile super hero*/
+router.get('/api/superhero', user.profileSuperHero);
+/* localhost:3000/api/home | go to home page */
+router.get('/api/home', user.profileSuperHero);
+
 
 
 module.exports = router;
